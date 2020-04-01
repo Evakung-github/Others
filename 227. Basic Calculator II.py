30498533f5b768141e772e174bfa60e61a2432db
Evakung-github <58772659+Evakung-github@users.noreply.github.com>
2020-04-01 11:08:10 -0700

    Update 227. Basic Calculator II.py

--- 227. Basic Calculator II.py
+++ 227. Basic Calculator II.py
@@ -1,3 +1,4 @@
+#First try. Works but slow.
 class Solution:
     def calculate(self, s: str) -> int:
         s+='+'
@@ -37,3 +38,50 @@ class Solution:
             i+=2
         
         return sum
+
+# It is noticed that I only need to track the current sign and I don't need to store every sign before it.
+# Also, I could transfer '-' to plus negative interger.
+# The faster implementation is as follow.
+
+class Solution:
+    def calculate(self, s: str) -> int:
+        s+='+'
+        if len(s) == 0:
+            return 0
+        stack = []
+        cur = 0
+        sign = '+'
+        i = 0
+        while i < len(s):
+            if s[i] == ' ':
+                i+=1
+                continue
+            if s[i] not in ['+','-','*','/']:
+                cur = cur*10 + int(s[i])
+            else:
+                if sign == '+':
+                    stack.append(cur)
+                elif sign == '-':
+                    stack.append(-cur)
+                else:
+                    pre = stack.pop()
+                    if sign == '*':
+                        stack.append(pre*cur)
+                    else:
+                        stack.append(int(pre/cur))
+                
+                sign = s[i]
+                cur = 0
+            i+=1
+
+        # print(stack)
+        
+        return sum(stack)
+    
+    
+    
+    
+    
+    
+    
+    
