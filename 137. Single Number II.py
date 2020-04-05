e6ec245e2324cd221ec617888511342b7320b0e5
Evakung-github <58772659+Evakung-github@users.noreply.github.com>
2020-04-05 13:32:10 -0700

    Create 137. Single Number II.py

---
+++ 137. Single Number II.py
@@ -0,0 +1,31 @@
+'''
+solution 1: count the total number of 1 at ith position. Since other numbers appear three times, 
+so the remainder after dividing total count by 3 is that single number.
+As the maximum size of integer is 2^32-1, so set the outer loop to be 32.
+Need to be aware of negative integer.
+However, the solution is slow. so I implemented another solution.
+'''
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ans = 0
+        s = 0
+        
+        for n in nums:
+            if n < 0: s+=1
+        
+        for i in range(32):
+            c = 0
+            for n in nums:
+                c += (1&(abs(n)>>i))
+            ans = ans | ((c%3) << i)
+            # ans += ((c%3) << i)
+        
+        return ans if s % 3== 0 else -ans
+
+    def singleNumber(self, nums: List[int]) -> int:
+#         ones,twos = 0,0
+#         for n in nums:
+#             ones = (ones ^ n) & ~twos
+#             twos = (twos ^ n) & ~ones
+        
+#         return ones
