a94e55b9ccf23643d540b4f91f304fdc48e54a7b
Evakung-github <58772659+Evakung-github@users.noreply.github.com>
2020-04-04 21:53:12 -0700

    Update 46. Permutations.py

--- 46. Permutations.py
+++ 46. Permutations.py
@@ -1,4 +1,16 @@
+# Time complexity:O(n!) factorial time
+
+
 class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        ans = []                
+        for i in range(len(nums)):
+            temp = [nums[i]]
+            l = self.permute(nums[:i]+nums[i+1:])
+            ans.extend(temp+i for i in l)
+        
+        return ans 
+
 #     def permute(self, nums: List[int]) -> List[List[int]]:
 #         if nums == None or len(nums) in[0,1]:
 #             return [nums]
@@ -13,29 +25,21 @@ class Solution:
 #                 ans.append([i]+j)
 #         return ans
 
-    def permute(self, nums: List[int]) -> List[List[int]]:
-        # if not nums:
-        #     return nums
-        # if len(nums) == 1:
-        #     return [nums]
-        ans = []
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         # if not nums:
+#         #     return nums
+#         # if len(nums) == 1:
+#         #     return [nums]
+#         ans = []
         
-        def helper(cur,nums):
-            if nums:
-                for i in range(len(nums)):
-                    helper(cur+[nums[i]],nums[:i]+nums[i+1:])
-            else:
-                ans.append(cur)
-                return
-        helper([],nums)
+#         def helper(cur,nums):
+#             if nums:
+#                 for i in range(len(nums)):
+#                     helper(cur+[nums[i]],nums[:i]+nums[i+1:])
+#             else:
+#                 ans.append(cur)
+#                 return
+#         helper([],nums)
         
-        return ans
-                    
-    def permute(self, nums: List[int]) -> List[List[int]]:
-        ans = []                
-        for i in range(len(nums)):
-            temp = [nums[i]]
-            l = self.permute(nums[:i]+nums[i+1:])
-            ans.extend(temp+i for i in l)
-        
-        return ans
+#         return ans
+
