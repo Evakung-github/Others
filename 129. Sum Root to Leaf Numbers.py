4da95ce17376e735aee13d47f09e4e42931f1496
Evakung-github <58772659+Evakung-github@users.noreply.github.com>
2020-04-06 10:56:02 -0700

    Update 129. Sum Root to Leaf Numbers.py

--- 129. Sum Root to Leaf Numbers.py
+++ 129. Sum Root to Leaf Numbers.py
@@ -9,9 +9,9 @@ class Solution:
     def sumNumbers(self, root: TreeNode) -> int:
         if not root:
             return 0 
+        # Using self.ans, then there is no need for return in helper function, which increases efficiency as it takes time to return value.
         self.ans = 0
         def helper(s,root):
-            ans = 0
 
             if not root.left and not root.right:
                 self.ans += s*10+root.val
@@ -21,6 +21,7 @@ class Solution:
                     helper(s*10+root.val,root.left)   
                 if root.right:
                     helper(s*10+root.val,root.right)
+        
         helper(0,root)    
         
         return self.ans
