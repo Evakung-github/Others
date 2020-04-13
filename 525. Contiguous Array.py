45d167df99d4d740862136062c2c3e8fbaff121f
Evakung-github <58772659+Evakung-github@users.noreply.github.com>
2020-04-13 10:42:38 -0700

    Update 525. Contiguous Array.py

--- 525. Contiguous Array.py
+++ 525. Contiguous Array.py
@@ -1,3 +1,14 @@
+'''
+I only track the number of ones, increase 1 when it's one, decrease 1 otherwise.
+The main idea is that when the counts of one are the same at different timestamp,
+it means that there is contguous array between them.
+(Draw a graph of the number of ones will be clearer.)
+Therefore, I use hashmap to track the position of different count (note that only the first position is stored.)
+Time complexity:O(n)
+
+'''
+
+
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
         ans = 0
