791105993e37f00b1f4c64fb01448e69deff8a1e
Evakung-github <58772659+Evakung-github@users.noreply.github.com>
2020-04-19 12:32:52 -0700

    Update 33&81. Search in Rotated Sorted Array.py

--- 33&81. Search in Rotated Sorted Array.py
+++ 33&81. Search in Rotated Sorted Array.py
@@ -1,3 +1,27 @@
+'''
+Q81 is a followup of Q33, where Q81 allows duplicate and Q33 doesn't.
+
+Let's discuss Q33 first.
+As the question requires us to do in O(logn), it is solved using binary search.
+
+There are two cases of mid, for each case we can split into two another cases.
+1. mid is in the ascending part of start
+  a. if target is in between start and mid, then we move right pointer to mid.
+  b. else: move left pointer to mid
+2. mid is in the descending part.
+  a. if target is in between mid and end, then we move left pointer to mid.
+  b. else: move right pointer to mid
+(it will be easier to understand if drawing images)
+
+However, things get a little bit complicated when duplicates are allowed.
+In this case, we might not be able to recognize which part mid is in.
+Hence, I think of a solution that first check whether left and right pointers point to the same value.
+Then do the same thing as in Q33.
+
+The Time complexity is at worst O(n) if all the value in the list are the same, but still O(logn) in general.
+'''
+
+
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
         # if target in set(nums):
