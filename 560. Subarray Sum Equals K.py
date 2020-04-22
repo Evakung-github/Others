17d720e8dc9bc49b6b0a6a08b88a3c0977468db7
Evakung-github <58772659+Evakung-github@users.noreply.github.com>
2020-04-22 11:20:01 -0700

    Update 560. Subarray Sum Equals K.py

--- 560. Subarray Sum Equals K.py
+++ 560. Subarray Sum Equals K.py
@@ -1,3 +1,28 @@
+'''
+The intuitive solution is iterating all the possible combinations(subarrays).
+Time complexity: O(n**2) which causes TLE.
+
+The improved algorithm is O(N).
+Starting from left, generate the accumulative sum and store it in a dictionary (hash map).
+The idea is presented below.
+
+sum(i,j) = sum(0,j) = sum(0,i-1)
+The dictionary is initiated with a zero
+Suppose array [-1,1,-1,1,1] ,k = 0.
+The accumulation array is [-1,0,-1,0,1].
+-1 --> store it.
+0  --> (k-0) in the dictionary, ans += 1 and store it, so there are 2 zeros now.
+-1 --> (k-(-1)) not in dictionary, store it, two -1 now.
+0  --> (k-0) in the dictionary, ans += 2 and store it, so there are 3 zeros now.
+(two zeors in the dictionary means that there are two start points to the current position with the total sum equal to k)
+....
+and so on.
+
+
+Then the answer is derived.
+'''
+
+
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         ans = 0
