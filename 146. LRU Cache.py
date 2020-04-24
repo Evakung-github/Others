c9beb9476e5ecfbcbdffa1572f31fdca5ba53bdb
Evakung-github <58772659+Evakung-github@users.noreply.github.com>
2020-04-24 11:00:42 -0700

    Update 146. LRU Cache.py

--- 146. LRU Cache.py
+++ 146. LRU Cache.py
@@ -1,4 +1,13 @@
+'''
+This problem is sloved by double-linked lists and hash table.
+Hash table helps us to find whether the element is in the cache for O(1) time complexity.
+Link list help us to track the order (recently used) and remove the items in O(1) time complexity.
+If using array, it requires O(N) time to remove.
 
+The idea is to use hash table to track the node, 
+and each node has prev and next features so that we can access to its previous node and next node when deleting it.
+ Also we need to know the head and the tail of the linked list to avoid unnecessary traversal of the linked lists.
+'''
 class doubleLink():
     def __init__(self,k,v):
         self.key = k
