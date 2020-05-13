457e3202f6a41b3593f962b3e6011e35ed9c5052
Evakung-github <58772659+Evakung-github@users.noreply.github.com>
2020-05-12 19:05:55 -0700

    quick sort, pivot

--- 215. Kth Largest Element in an Array.py
+++ 215. Kth Largest Element in an Array.py
@@ -1,3 +1,12 @@
+'''
+The problem is solved by quick sort.
+As it is unnecessary to find the first, the second, the third ...... largest, we need to eliminate excessive sort.
+Therefore, an index is picked up randomly and it is the point we used to separate the list, which is also called pivot.
+After one while loop, the numbers in left part are no less than the pivot.
+The worst time complexity is O(n^2) if it is the increasing or decreasing list. (The position of the pivot doesn't change and we have to iterate through the whole list)
+The best time complexity is O(n) if the medium is picked in every iteration.
+'''
+
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         l,r = 0,len(nums)-1
